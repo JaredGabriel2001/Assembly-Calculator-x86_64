6feaf4bc81ea8897383d7b448498b43eb6bec3d5
jaredgabriel2001 <gabrieljaredjared@gmail.com>
2025-03-09 00:27:13 -0300

    cheio de erro

--- calculadora.asm
+++ calculadora.asm
@@ -1,80 +1,115 @@
+; nasm -f elf64 calculadora.asm ; gcc -m64 -no-pie calculadora.o -o calculadora.x
+
 section .data
-    arg1  : db 10, 'Primeiro argumento (float): '
-    strl2 : equ $-arg1
-    arg2  : db 10, 'Segundo argumento (operacao): '
-    strl3 : equ $-arg2
-    arg3  : db 10, 'Terceiro argumento (float): '
-    strl4 : equ $-arg3
-    lf    : db 10,10 ; quebra-de-linha dupla
+    output1 db "Primeiro operando: %.2lf", 10, 0
+    output2 db "Operador: %c", 10, 0
+    output3 db "Segundo operando: %.2lf", 10, 0
+    resultOutput db "Resultado: %.2lf", 10, 0
+    formatFloat db "%lf", 0
+    formatChar db " %c", 0
+    resultFormat db "%.2lf + %.2lf = %.2lf", 10, 0
+    filename db "saida.txt", 0
+    printArgsFormat db "Argumentos: %.2lf %c %.2lf", 10, 0
 
 section .bss
-    temp  : resb 1  ; char para impressão
-    noPa  : resq 1  ; número de parâmetros
+    operando1 resq 1
+    operando2 resq 1
+    operador resb 1
+    resultado resq 1
+    file resq 1
 
 section .text
-    global _start
+    global main
+    extern printf, sscanf, fopen, fprintf, fclose
 
-_start:
-    xor r9, r9
-    pop qword [noPa]  ; número de parâmetros -- argc
+main:
+    ; Prolog
+    push rbp
+    mov rbp, rsp
 
-    ; Ignorar o nome do programa
-    pop r15           ; Remove o primeiro argumento (nome do programa)
+    ; Carregar argc e argv
+    mov rdi, [rbp+16] ; argc
+    cmp rdi, 4          ; Verifica se argc == 4 (nome do programa + 3 argumentos)
+    jne end             ; Se não for, termina silenciosamente
 
-    ; Processa o primeiro argumento (float)
-    pop r15           ; Primeiro número float
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, arg1
-    mov rdx, strl2
-    syscall
+    ; Processar argumentos
+    mov rsi, [rbp+24] ; argv
+    mov rsi, [rsi+8]  ; argv[1]
+    mov rdi, formatFloat
+    lea rdx, [operando1]
+    call sscanf        ; Lê o primeiro operando
+    cmp rax, 1
+    jne end
 
-    call printArg
+    mov rsi, [rbp+24]
+    mov rsi, [rsi+16] ; argv[2]
+    mov rdi, formatChar
+    lea rdx, [operador]
+    call sscanf        ; Lê o operador
+    cmp rax, 1
+    jne end
 
-    ; Processa o segundo argumento (operador)
-    pop r15           ; Operação (char: a, s, m, d)
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, arg2
-    mov rdx, strl3
-    syscall
+    mov rsi, [rbp+24]
+    mov rsi, [rsi+24] ; argv[3]
+    mov rdi, formatFloat
+    lea rdx, [operando2]
+    call sscanf        ; Lê o segundo operando
+    cmp rax, 1
+    jne end
 
-    call printArg
+    ; Print dos argumentos
+    mov rdi, printArgsFormat
+    movsd xmm0, [operando1]
+    movzx esi, byte [operador]
+    movsd xmm1, [operando2]
+    call printf
 
-    ; Processa o terceiro argumento (float)
-    pop r15           ; Segundo número float
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, arg3
-    mov rdx, strl4
-    syscall
+    ; Exibir os valores lidos
+    mov rdi, output1
+    movsd xmm0, [operando1] ; Passa operando1 no xmm0 para printf
+    call printf
 
-    call printArg
+    mov rdi, output2
+    movzx rsi, byte [operador] ; Passa operador no rsi para printf
+    call printf
 
-    ; Finaliza o programa
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, lf
-    mov rdx, 2
-    syscall
+    mov rdi, output3
+    movsd xmm0, [operando2] ; Passa operando2 no xmm0 para printf
+    call printf
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    ; Realizar a soma se o operador for 'a'
+    movzx rax, byte [operador]
+    cmp rax, 'a'
+    jne end_calc
 
-printArg:
-    xor r9, r9
-printLoop:
-    mov r8b, [r15 + r9]
-    cmp r8b, 0
-    je endPrint
-    mov [temp], r8b
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, temp
-    mov rdx, 1
-    syscall
-    inc r9
-    jmp printLoop
-endPrint:
+    movsd xmm0, [operando1]
+    addsd xmm0, [operando2]
+    movsd [resultado], xmm0
+
+    ; Abrir o arquivo para escrita (append)
+    mov rdi, filename
+    mov rsi, "a"
+    call fopen
+    cmp rax, 0
+    je end_calc
+    mov [file], rax
+
+    ; Escrever o resultado no arquivo
+    mov rdi, [file]
+    mov rsi, resultFormat
+    movsd xmm0, [operando1]
+    movsd xmm1, [operando2]
+    movsd xmm2, [resultado]
+    call fprintf
+
+    ; Fechar o arquivo
+    mov rdi, [file]
+    call fclose
+
+end_calc:
+    ; Epilog
+    xor rax, rax
+end:
+    mov rsp, rbp
+    pop rbp
     ret
